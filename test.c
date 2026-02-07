6727d002d0a0fdb451d52a1a494233e7296fb989
lucky <1210833823@qq.com>
2026-02-07 19:38:12 +0800

    Style

--- test.c
+++ test.c
@@ -2,14 +2,14 @@
 
 int main(int argc, char **argv)
 {
-	int a;
-	
-	a = 10;
-	printf("hello world \n");
-	
-	a = 20;
-	
-	return 0;
+    int a;
+
+    a = 10;
+    printf("hello world \n");
+
+    a = 20;
+
+    return 0;
 }
 
 int func(int a)
