a7ae9e1e618adbb3e9dd10b66beecca09ea78e02
lucky <1210833823@qq.com>
2026-05-23 14:20:17 +0800

    Merge and Clean

--- RA4M2/LED/build/debug/CMakeFiles/open_rasc_LED.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/open_rasc_LED"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/open_rasc_LED.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
